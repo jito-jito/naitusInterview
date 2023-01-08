2c7b1676762c5d3a4afc548086d04e7090ba235c
jito <sergio.sergio.98@hotmail.com>
2023-01-07 21:30:37 -0300

    feat(!environment, routing, components): change bootstrap minified import to add a complete version of the library to change and add utilities.
    Add icon of the page and font type in root HTML.
    
    Create pages and components folders with the initial config of components and add header component with styles and routing working.
    
    Add configuration to enable routing around 'personajes' and 'episodios' pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'personajes',
+    title: 'Rick&Morty',
+    pathMatch: 'full'
+  },
+  {
+    path: 'personajes',
+    title: 'Rick&Morty',
+    component: CharactersComponent
+  },
+  {
+    path: 'episodios',
+    title: 'Rick&Morty',
+    component: EpisodesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CharacterComponent } from './components/character/character.component';
+import { EpisodeComponent } from './components/episode/episode.component';
+import { CharactersComponent } from './pages/characters/characters.component';
+import { EpisodesComponent } from './pages/episodes/episodes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CharacterComponent,
+    EpisodeComponent,
+    CharactersComponent,
+    EpisodesComponent
   ],
   imports: [
     BrowserModule,
